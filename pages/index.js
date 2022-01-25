b81dee1403c450761969e6e02c085f3b53357561
Daniel.Reis <73089512+Danny-codes@users.noreply.github.com>
2022-01-25 11:50:17 -0300

    Getting user profile through input
    Getting user profile through input and stabilishing _app.js with the global styles

--- pages/index.js
+++ pages/index.js
@@ -1,36 +1,8 @@
 import appConfig from '../config.json'
 import { Box, Button, Text, TextField, Image} from '@skynexui/components'
+import {useState} from 'react'
+import { useRouter } from 'next/router'
 
-function GlobalStyle() {
-  return (
-    <style global jsx>{`
-      * {
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-        list-style: none;
-      }
-      body {
-        font-family: "Open Sans", sans-serif;
-      }
-      /* App fit Height */
-      html,
-      body,
-      #__next {
-        min-height: 100vh;
-        display: flex;
-        flex: 1;
-      }
-      #__next {
-        flex: 1;
-      }
-      #__next > * {
-        flex: 1;
-      }
-      /* ./App fit Height */
-    `}</style>
-  );
-}
 
 function Titulo(props) {
   const value = props.children;
@@ -60,11 +32,12 @@ function Titulo(props) {
 //export default HomePage;
 
 export default function PaginaInicial() {
-    const username = 'Danny-codes';
+    //const username = 'Danny-codes';
+    const [username, setUsername] = useState('')
+    const roteamento = useRouter();
   
     return (
       <>
-        <GlobalStyle />
         <Box
           styleSheet={{
             display: 'flex', alignItems: 'center', justifyContent: 'center',
@@ -91,6 +64,10 @@ export default function PaginaInicial() {
             {/* Formulário */}
             <Box
               as="form"
+              onSubmit={function (event) {
+                event.preventDefault();
+                roteamento.push('/chat')
+              }}
               styleSheet={{
                 display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
                 width: { xs: '100%', sm: '50%' }, textAlign: 'center', marginBottom: '32px',
@@ -101,7 +78,19 @@ export default function PaginaInicial() {
                 {appConfig.name}
               </Text>
   
+             {/* <input 
+                type="text" 
+                value={username} 
+                onChange={function handler(event){
+                  //Pegando valor digitado
+                  const valor = event.target.value
+                  //trocar o valor da variavel atraves da função
+                  setUsername(valor)
+                }}
+              />*/}
               <TextField
+                value={username} 
+                placeholder="GitHub username"
                 fullWidth
                 textFieldColors={{
                   neutral: {
@@ -111,7 +100,11 @@ export default function PaginaInicial() {
                     backgroundColor: appConfig.theme.colors.neutrals[800],
                   },
                 }}
-              />
+                onChange={function handler(event){
+                  const valor = event.target.value
+                  setUsername(valor)
+                }}
+              /> 
               <Button
                 type='submit'
                 label='Entrar'
